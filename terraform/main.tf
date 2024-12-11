787c318179194b08bc253df528c5f00f0c616fb0
acikgozb <berkacikgoz@armut.com>
2024-12-11 11:54:36 +0300

    [infra] Add site bucket and policy resources
    The IaC module is updated to contain site bucket and policy resources.
    
    Since the policy contains Cloudflare's possible proxy CIDR values, a
    script is implemented as an external data source to retrieve the values.

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,50 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-//NOTE: acikgozb - Is it possible to pass region via env vars?
-provider "aws" {
-  region = "eu-west-2"
+provider "aws" {}
+
+locals {
+  bucket_id   = "acikgozb-dev"
+  application = "acikgozb.dev"
 }
 
+resource "random_bytes" "bucket_id" {
+  keepers = {
+    bucket_id = local.bucket_id
+  }
+
+  length = 6
+}
+
+resource "aws_s3_bucket" "acikgozb-dev" {
+  bucket = "${local.bucket_id}-${random_bytes.bucket_id.hex}"
+  tags = {
+    Name        = "site-bucket"
+    application = local.application
+  }
+}
+
+data "external" "cloudflare-cidr-ranges" {
+  program = ["bash", "${path.module}/scripts/cloudflare-proxy-cidr"]
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions = ["s3:GetObject"]
+
+    resources = [aws_s3_bucket.acikgozb-dev.arn]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = jsondecode(data.external.cloudflare-cidr-ranges.result.list)
+    }
+  }
+
+  depends_on = [data.external.cloudflare-cidr-ranges]
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.acikgozb-dev.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
