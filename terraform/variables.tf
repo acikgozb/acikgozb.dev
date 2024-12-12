b03c5ee5a999bfc9fa181e27e039986474235054
acikgozb <berkacikgoz@armut.com>
2024-12-12 16:24:45 +0300

    [infra] Fix app_name regex validation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "app_name" {
   description = "The name of the application that is being provisioned by Terraform."
 
   validation {
-    condition     = can(regex("^[[:space:]]+$", var.app_name))
+    condition     = can(regex("[^[:space:]]", var.app_name))
     error_message = "The variable 'app_name' cannot contain whitespaces."
   }
 }
